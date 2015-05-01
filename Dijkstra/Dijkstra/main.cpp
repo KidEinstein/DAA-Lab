1360db9bcb9e1dc3e027ac989a9e953d91d79218
KidEinstein <anirudhshekh@gmail.com>
2015-05-01 12:49:52 +0530

    Added Kruskal

--- Dijkstra/Dijkstra/main.cpp
+++ Dijkstra/Dijkstra/main.cpp
@@ -16,8 +16,8 @@ void readGraph() {
     for (int i = 0; i < size; i++)
         adj_matrix[i] = new int[size];
     
-    for (int i = 0; i < size; i++){
-        for (int j = 0; j < size; j++){
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
             adj_matrix[i][j] = INFINIT;
             if (i == j){
                 adj_matrix[i][j] = 0;
@@ -30,15 +30,15 @@ void readGraph() {
     cin>>n;
     cout<<"Enter the edges of the graph: \n";
     
-    while (n--){
+    while (n--) {
         cin>>i>>j>>l;
         adj_matrix[i][j] = l;
     }
 }
 
 void displayGraph() {
-    for (int i = 0; i < size; i++){
-        for (int j = 0; j < size; j++){
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
             cout<<adj_matrix[i][j]<<"\t\t";
         }
         cout<<"\n";
@@ -56,7 +56,7 @@ void dijkstra() {
     cout<<"Enter source vertex: ";
     cin>>src;
     
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++) {
         arr[i] = INFINIT;
         done[i] = false;
         shortest_path[i] = to_string(src);
@@ -64,34 +64,33 @@ void dijkstra() {
     
     arr[src] = 0;
     
-    for (int i = 0; i < size - 1 ; i++){
+    for (int i = 0; i < size - 1 ; i++) {
         mini = -1;
         
-        for (int j = 0; j < size; j++){
+        for (int j = 0; j < size; j++) {
             if (done[j])
                 continue;
             
-            if (mini == -1){
+            if (mini == -1) {
                 mini = j;
             }
             
-            if (arr[mini] > arr[j] && !done[j]){
+            if (arr[mini] > arr[j] && !done[j]) {
                 mini = j;
             }
         }
         
         done[mini] = true;
         
-        for (int j = 0; j < size; j++){
+        for (int j = 0; j < size; j++) {
             if (arr[mini] + adj_matrix[mini][j] < arr[j]) {
                 arr[j] = arr[mini] + adj_matrix[mini][j];
                 shortest_path[j] = shortest_path[mini] + " --> " + to_string(j);
             }
         }
     }
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++) {
         cout<<arr[i]<<"\t"<<shortest_path[i]<<"\n";
-        
     }
     
 }
---
+++ Kruskal/Kruskal/main.cpp
@@ -0,0 +1,131 @@
+//
+//  main.cpp
+//  Kruskal
+//
+//  Created by Anirudh Shekhawat on 01/05/15.
+//  Copyright (c) 2015 Anirudh Shekhawat. All rights reserved.
+//
+
+//Uses weighted quick union
+
+#include <iostream>
+#define INFINIT 9999
+using namespace std;
+
+struct Edge {
+    int u, v;
+    int weight;
+    bool selected;
+};
+
+Edge *e;
+int num_nodes;
+int num_edges;
+int** adj_matrix;
+
+class QuickUnionUF {
+    int *id;
+    int *size;
+public:
+    QuickUnionUF(int sz) {
+        id = new int[sz];
+        size = new int[sz];
+        for (int i = 0; i < sz; i++) {
+            id[i] = i;
+            size[i] = 1;
+        }
+        
+    }
+    
+    int root(int i) {
+        while (i != id[i]) {
+            i = id[i];
+        }
+        return i;
+    }
+    
+    bool connected(int p, int q) {
+        return root(p) == root(q);
+    }
+    
+    void doUnion(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        if (size[i] > size[j]) {
+            id[j] = i;
+            size[i] += size[j];
+        }
+        else {
+            id[i] = j;
+            size[j] += size[i];
+        }
+    }
+};
+
+
+void readGraph() {
+    cout<<"Enter number of vertices: ";
+    cin>>num_nodes;
+    int u, v, w;
+    cout<<"Enter number of edges";
+    cin>>num_edges;
+    cout<<"Enter the edges of the graph: \n";
+    e = new Edge[num_edges];
+    for (int i = 0; i < num_edges; i++) {
+        cin>>u>>v>>w;
+        e[i].u = u;
+        e[i].v = v;
+        e[i].weight = w;
+    }
+}
+
+void sort(Edge e[]) {
+    int min_index;
+    for (int i = 0; i < num_edges; i++) {
+        min_index = i;
+        for (int j = i + 1; j < num_edges; j++) {
+            if (e[j].weight < e[min_index].weight) {
+                min_index = j;
+            }
+            Edge temp = e[i];
+            e[i] = e[min_index];
+            e[min_index] = temp;
+        }
+    }
+}
+void kruskal() {
+    sort(e);
+    QuickUnionUF uf(num_nodes);
+    for (int i = 0; i < num_edges; i++) {
+        cout<<e[i].u<<"\t"<<e[i].v<<"\t"<<e[i].weight<<"\n";
+        e[i].selected = false;
+    }
+    int n = 0, i = 0;
+    e[i].selected = true;
+    uf.doUnion(e[i].u, e[i].v);
+    i++;
+    n++;
+    while (n != num_nodes - 1) {
+        if (!uf.connected(e[i].u, e[i].v)) {
+            uf.doUnion(e[i].u, e[i].v);
+            e[i].selected = true;
+            n++;
+        }
+        i++;
+    }
+    
+    for (int i = 0; i < num_edges; i ++) {
+        if (e[i].selected) {
+            cout<<e[i].u<<"\t"<<e[i].v<<"\n";
+        }
+    }
+    
+}
+
+int main() {
+    readGraph();
+    kruskal();
+}
+
+
+
